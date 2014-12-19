883c462d167c48c5d624b031d7462e65907785f0
runger01 <robin.e.unger@gmail.com>
2014-12-19 14:17:25 -0700

    added ability to generate a new canvas of user's chosen size

--- etch.js
+++ etch.js
@@ -1,36 +1,46 @@
-var generateCanvas = function(width, height) {
+var generateCanvas = function(canvasSize, cellSize) {
     //generate canvas columns followed by canvas rows(cells)
-    for(var i = 0; i < width; i++) {
+    for(var i = 0; i < canvasSize; i++) {
         $("#canvas").append("<div class='column'></div>");
     }
-    for(var i = 0; i < height; i++) {
+    for(var i = 0; i < canvasSize; i++) {
         $(".column").append("<div class='cell'></div>");
     }
+    // fit the grid to the canvas (720x720)
+    $(".cell").css("width", cellSize);
+    $(".cell").css("height", cellSize);
 }
 
-var clearCanvas = function() {
-	$(".column").remove();
-};
-
 var paintCell = function() {
+    var red =   "#ee6363";
+    var green = "#63ee63";
+    var blue =  "#6363ee";
+
     $(".cell").hover(
         function(){
-            $(this).addClass("blue");
+            $(this).css("background", "orange");
         },
         function(){
-            $(this).addClass("light_blue");
+            $(this).css("background", "orange");
         }
     );
 };
 
 $(document).ready(function() {
     // default canvas
-    generateCanvas(16, 16);
+    generateCanvas(16);
     paintCell();
 
-    $("button[name=button_clear]").click(function() {
+    $("button[name=clear_canvas]").click(function() {
         $(".column").remove();
-        generateCanvas(16, 16);
+        generateCanvas(16);
+        paintCell();
+    });
+    $("button[name=new_canvas]").click(function() {
+        $(".column").remove();
+        var canvasSize = prompt("Enter a new canvas size:");
+        cellSize = (720/canvasSize);
+        generateCanvas(canvasSize, cellSize);
         paintCell();
     });
 });
